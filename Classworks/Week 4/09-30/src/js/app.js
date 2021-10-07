0a23964bf50cda768c1c9c3e8a348b3254f01b45
Ben Ju <benas.juskus@gmail.com>
2021-10-07 15:26:34 +0300

    Homework 09-30

--- Classworks/Week 4/09-30/src/js/app.js
+++ Classworks/Week 4/09-30/src/js/app.js
@@ -1,15 +1,12 @@
-// let  first = prompt('please insert the first number'),
-//     second = prompt('please insert the second number'),
-//     sum = parseInt(first) + parseInt(second);
-// console.log(sum);
-//
-//
-// document.write('the sum is', ' ', sum);
+let btnFilter = document.getElementById('filter');
 
-let first = (Math.random() * 10).toFixed(2),
-    second = (Math.random() * 10).toFixed(2),
-    third = (Math.random() * 10).toFixed(2),
-    fourth = (Math.random() * 10).toFixed(2),
-    fifth = (Math.random() * 10).toFixed(2);
-console.log(first, second, third, fourth, fifth);
-console.log('The highest value is ', Math.max(first, second, third, fourth, fifth), ' and the lowest value is ', Math.min(first, second, third, fourth, fifth));
+btnFilter.addEventListener('click', function() {
+    let list = document.getElementById('expanded');
+    if (list.style.display === 'none') {
+        list.style.display = 'block';
+    } else {
+        list.style.display = 'none';
+    }
+})
+
+console.log(btnFilter);
