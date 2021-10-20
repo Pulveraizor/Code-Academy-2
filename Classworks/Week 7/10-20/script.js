f7806f65e358120c43940a08bc84136b2174b486
Ben Ju <benas.juskus@gmail.com>
2021-10-20 21:47:53 +0300

    Classwork 10-20

---
+++ Classworks/Week 7/10-20/script.js
@@ -0,0 +1,44 @@
+//[21:28] Kirilas ČalkinDOM navigacija
+//
+//
+// Sukurkite HTML dokumentą su lentele (table) ir forma (form). Dokumentu turi būti žemiau aptartos žymos (angl. tags) ir jų atributai.
+//
+//
+//
+// Suraskite ir konsolėje atvaizduokite:
+//
+//
+//
+// Lentelę su id = "age-table".
+
+let ageTable = document.getElementById('age-table');
+
+console.log(ageTable);
+
+// Pirmasis <td> toje lentelėje (su id “age”).
+
+let age = document.querySelector('#age > td')
+
+console.log(age);
+
+// Visus <label> elementus lentelės viduje (jų turėtų būti 3).
+
+let label = document.querySelectorAll('label');
+
+console.log(label);
+
+// Pirmasis įvestį (<input>) į formą.
+
+let firstInput = document.querySelector('input');
+
+console.log(firstInput);
+
+// Paskutinę įvestį (<input>) į formą.
+
+let lastInput = document.querySelector('input:last-of-type');
+
+console.log(lastInput);
+
+
+
+
