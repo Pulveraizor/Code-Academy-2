e39c8a63f2829885c2716c0089ee91f40f6ccf0f
Ben Ju <benas.juskus@gmail.com>
2021-11-03 20:04:23 +0200

    Classwork 11-03

--- Classworks/Week 9/11-03/script.js
+++ Classworks/Week 9/11-03/script.js
@@ -1,4 +1,7 @@
-let film = new Movie('From Dusk till Dawn', 1996, 'Robert Rodriguez', 19000000, 59300000);
+let film = new Movie('From Dusk till Dawn', 1996, 'Robert Rodriguez', 19000000, 59300000),
+    film2 = new Movie('Inglourious Basterds', 2009, 'Quentin Tarantino', 70000000, 321500000);
 
 console.log(film.getIntroduction());
 console.log(film.getProfit());
+console.log(film2.getIntroduction());
+console.log(film2.getProfit());
