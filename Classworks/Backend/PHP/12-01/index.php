95766c5e0ada2e3b59af10a22eefd9b661eacb19
Ben Ju <benas.juskus@gmail.com>
2021-12-01 18:43:34 +0200

    class 12-01

--- Classworks/Backend/PHP/12-01/index.php
+++ Classworks/Backend/PHP/12-01/index.php
@@ -5,3 +5,12 @@
 $secondsInADay = 60 * 60 * 24;
 
 echo "A day has $secondsInADay seconds in it";
+
+echo '<br>';
+
+// Sukurkite kintamąjį $hello ir priskirkite jam reikšmę „hello“. Naudodami atskirus šios eilutės simbolius, ekrane atspausdinkite:
+//„h“, „e“, „o“.
+
+$hello = 'hello';
+
+echo "\"$hello[0]\", \"$hello[1]\", \"$hello[4]\"";
