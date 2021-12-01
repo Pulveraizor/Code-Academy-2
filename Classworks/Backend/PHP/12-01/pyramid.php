877a5e12889b57d98fd4dc81a45a17b8cdf2c69e
Ben Ju <benas.juskus@gmail.com>
2021-12-01 20:28:12 +0200

    Task 4

--- Classworks/Backend/PHP/12-01/pyramid.php
+++ Classworks/Backend/PHP/12-01/pyramid.php
@@ -12,7 +12,16 @@
 // 999999999
 
 
-//Method 1;
+//Method 1:
 for ($i = 1; $i < 10; $i++) {
     echo str_repeat($i, $i), '<br>';
 }
+
+//Method 2:
+function createPyramid(...$arg) {
+    foreach ($arg as &$value) {
+        echo str_repeat($value, $value), '<br>';
+    }
+}
+
+createPyramid(1, 2, 3, 4, 5, 6, 7, 8, 9);
