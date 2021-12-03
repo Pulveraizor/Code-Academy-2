3a365551a79bde43611098544746345be8d996fa
Ben Ju <benas.juskus@gmail.com>
2021-12-03 15:59:27 +0200

    Books

--- Classworks/Backend/PHP/12-01/books.php
+++ Classworks/Backend/PHP/12-01/books.php
@@ -3,10 +3,10 @@
 // Sukurti masyvą, kuris aprašytų knygos duomenis: title, author, year, genre
 
 $book1 = [
-    'Title' => 'The Hobbit',
+    'Title'  => 'The Hobbit',
     'Author' => 'J. R. R. Tolkien',
-    'Year' => 1937,
-    'Genre' => 'Fantasy'
+    'Year'   => 1937,
+    'Genre'  => 'Fantasy'
 ];
 
 // Sukurti masyvą, kurio elementai būtų masyvai aprašantys knygas. Minimum 3.
@@ -14,22 +14,22 @@ $book1 = [
 $books = [
     $book1,
     [
-    'Title' => 'Harry Potter and the Philosopher\'s Stone',
+    'Title'  => 'Harry Potter and the Philosopher\'s Stone',
     'Author' => 'J. K. Rowling',
-    'Year' => 1997,
-    'Genre' => 'Fantasy'
+    'Year'   => 1997,
+    'Genre'  => 'Fantasy'
     ],
     [
-    'Title' => 'The Little Prince',
+    'Title'  => 'The Little Prince',
     'Author' => 'Antoine de Saint-Exupéry',
-    'Year' => 1943,
-    'Genre' => 'Novella'
+    'Year'   => 1943,
+    'Genre'  => 'Novella'
     ],
     [
-    'Title' => 'Dream of the Red Chamber',
+    'Title'  => 'Dream of the Red Chamber',
     'Author' => 'Cao Xueqin',
-    'Year' => 1800,
-    'Genre' => 'Family saga'
+    'Year'   => 1800,
+    'Genre'  => 'Family saga'
     ],
 
 ];
